0ae9ccd8dab2d8a8ef4125c97888fad3bb38220d
jollygrin <JollyGrin@protonmail.com>
2024-11-13 01:58:48 +0100

    stones

--- src/lib/components/Stone.svelte
+++ src/lib/components/Stone.svelte
@@ -2,15 +2,12 @@
   import { T } from "@threlte/core";
   import { CylinderGeometry } from "three";
   import { interactivity, useCursor } from "@threlte/extras";
-  import { tweened } from "svelte/motion";
-  import { cubicOut } from "svelte/easing";
 
   interactivity();
 
   export let color = 0xff0000; // Default stone color
   export let outlineColor = 0x000000; // Default outline color
 
-  // Radius and height values for a beveled disc-like shape
   const topRadius = 0.35;
   const bottomRadius = 0.3;
   const height = 0.15; // Very thin to give a disc-like appearance
@@ -21,13 +18,6 @@
   const { onPointerEnter, onPointerLeave } = useCursor();
 
   export let position: [number, number, number] = [0, 0.21, 0]; // Initial position of the stone
-
-  // let position = tweened(initialPosition, {
-  //   duration: 600,
-  //   easing: cubicOut,
-  // });
-  // // Watch the target position and animate when it changes
-  // $: position.set([1, 4, 0]);
 </script>
 
 <!-- Stone as a beveled cylinder -->
--- src/lib/components/Stones.svelte
+++ src/lib/components/Stones.svelte
@@ -3,7 +3,15 @@
   import { cubicOut } from "svelte/easing";
   import Stone from "$lib/components/Stone.svelte"; // Stone component for the token
 
+  const BLUE = 0x0f91db;
+  const RED = 0xdb0f35;
   let color = 0xff0000;
 </script>
 
-<Stone initialPosition={[0, 2, 0]} {color} />
+{#each Array.from({ length: 7 }).map((_, i) => i) as slotNumber}
+  <Stone position={[-3 + slotNumber, -0.05, 2]} color={RED} />
+{/each}
+
+{#each Array.from({ length: 7 }).map((_, i) => i) as slotNumber}
+  <Stone position={[-3 + slotNumber, -0.05, -2]} color={BLUE} />
+{/each}
