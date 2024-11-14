ee23ac6629dd0e141cc03e1e133774554ae11938
jollygrin <JollyGrin@protonmail.com>
2024-11-14 19:41:34 +0100

    add rossets

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -11,19 +11,31 @@
 
 <!-- Left Lane -->
 {#each Array.from({ length: 8 }).map((_, i) => i) as slotNumber}
-  {#if ![4, 5].includes(slotNumber)}
+  {#if [0, 6].includes(slotNumber)}
+    <Cube position={[-3 + slotNumber, 0, 1]} topFaceColor={0xdb0f35} />
+  {/if}
+
+  {#if ![0, 4, 5, 6].includes(slotNumber)}
     <Cube position={[-3 + slotNumber, 0, 1]} />
   {/if}
 {/each}
 
 <!-- Middle Lane -->
 {#each Array.from({ length: 8 }).map((_, i) => i) as slotNumber}
-  <Cube position={[-3 + slotNumber, 0, 0]} />
+  {#if ![3].includes(slotNumber)}
+    <Cube position={[-3 + slotNumber, 0, 0]} />
+  {:else}
+    <Cube position={[-3 + slotNumber, 0, 0]} topFaceColor={0xdb0f35} />
+  {/if}
 {/each}
 
 <!-- Right Lane -->
 {#each Array.from({ length: 8 }).map((_, i) => i) as slotNumber}
-  {#if ![4, 5].includes(slotNumber)}
+  {#if [0, 6].includes(slotNumber)}
+    <Cube position={[-3 + slotNumber, 0, -1]} topFaceColor={0xdb0f35} />
+  {/if}
+
+  {#if ![0, 4, 5, 6].includes(slotNumber)}
     <Cube position={[-3 + slotNumber, 0, -1]} />
   {/if}
 {/each}
